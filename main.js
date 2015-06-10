5545c00bd47b329f2e69977d7c37df06f1f0c7d0
Nick Bray <ncbray@chromium.org>
2015-06-10 16:20:23 -0700

    Support direct calls.

--- main.js
+++ main.js
@@ -5,14 +5,44 @@ var demo = {};
     var m = wasm.Module({
       funcs: [
 	wasm.Function({
-	  name: "foo",
-	  args: [],
+	  name: "bar",
+	  argCount: 1,
+	  returnType: "i32",
+	  locals: [
+	    "i32",
+	    "i32",
+	  ],
 	  body: [
 	    wasm.Return({
 	      expr: wasm.BinaryOp({
-		left: wasm.Const({value: 2}),
-		op: "-",
-		right: wasm.Const({value: 3}),
+		left: wasm.ConstI32({value: 7}),
+		op: "+",
+		right: wasm.GetLocal({index: 0}),
+	      }),
+	    }),
+	  ],
+	}),
+	wasm.Function({
+	  name: "foo",
+	  argCount: 0,
+	  returnType: "i32",
+	  locals: [
+	  ],
+	  body: [
+	    wasm.Return({
+	      expr: wasm.BinaryOp({
+		left: wasm.BinaryOp({
+		  left: wasm.ConstI32({value: 2}),
+		  op: "-",
+		  right: wasm.ConstI32({value: 3}),
+		}),
+		op: "*",
+		right: wasm.CallDirect({
+		  func: 0,
+		  args: [
+		    wasm.ConstI32({value: 5}),
+		  ],
+		}),
 	      }),
 	    }),
 	  ],
@@ -20,8 +50,10 @@ var demo = {};
       ],
     });
     var src = wasm.GenerateJS(m);
+    console.log(src);
     var compiled = eval(src);
-    console.log(compiled);
     console.log(compiled.foo());
+
+    demo.m = compiled;
   };
 })(demo);
--- wasm.js
+++ wasm.js
@@ -2,12 +2,24 @@ var wasm = {};
 
 (function(exports) {
 
-  exports.Const = function(args) {
-    return {type: "const", value: args.value};
+  exports.ConstI32 = function(args) {
+    return {type: "const_i32", value: args.value};
+  };
+
+  exports.ConstF32 = function(args) {
+    return {type: "const_f32", value: args.value};
+  };
+
+  exports.GetLocal = function(args) {
+    return {type: "getlocal", index: args.index};
   };
 
   exports.BinaryOp = function(args) {
     return {type: "binop", left: args.left, op: args.op, right: args.right};
+  };
+
+  exports.CallDirect = function(args) {
+    return {type: "calldirect", func: args.func, args: args.args};
   };
 
   exports.Return = function(args) {
@@ -15,11 +27,81 @@ var wasm = {};
   };
 
   exports.Function = function(args) {
-    return {type: "function", name: args.name, args: args.args, body: args.body};
+    return {
+      type: "function",
+      name: args.name,
+      argCount: args.argCount,
+      locals: args.locals,
+      returnType: args.returnType,
+      body: args.body
+    };
   };
 
   exports.Module = function(args) {
     return {type: "module", funcs: args.funcs};
+  };
+
+
+  var SemanticPass = function() {
+  };
+
+  SemanticPass.prototype.processExpr = function(expr) {
+    switch(expr.type) {
+    case "const_i32":
+      expr.etype = "i32";
+      return expr.etype;
+    case "const_f32":
+      expr.etype = "f32";
+      return expr.etype;
+    case "getlocal":
+      expr.etype = this.func.locals[expr.index];
+      return expr.etype;
+    case "binop":
+      var lt = this.processExpr(expr.left);
+      var rt = this.processExpr(expr.right);
+      if (lt != rt) {
+	console.log(expr, lt, rt);
+	throw expr;
+      }
+      expr.etype = lt;
+      return expr.etype;
+    case "calldirect":
+      var target = this.module.funcs[expr.func];
+      if (expr.args.length != target.argCount) {
+	console.log(target);
+	throw expr.args.length;
+      }
+      for (var i = 0; i < expr.args.length; i++) {
+	var arg = expr.args[i];
+	this.processExpr(arg);
+	if (arg.etype != target.locals[i]) {
+	  console.log(i, arg.etype, target.locals[i]);
+	  throw expr;
+	}
+      }
+      // TODO validate arguments.
+      expr.etype = target.returnType;
+      return expr.etype;
+    case "return":
+      this.processExpr(expr.expr);
+      return "void";
+    default:
+      throw expr.type;
+    }
+  };
+
+  SemanticPass.prototype.processFunction = function(func) {
+    this.func = func;
+    for (var i in func.body) {
+      this.processExpr(func.body[i]);
+    }
+  };
+
+  SemanticPass.prototype.processModule = function(module) {
+    this.module = module;
+    for (var i in module.funcs) {
+      this.processFunction(module.funcs[i]);
+    }
   };
 
   var CodeWriter = function() {
@@ -55,20 +137,64 @@ var wasm = {};
     return this;
   };
 
+  var JSGenerator = function(m) {
+    this.m = m;
+    this.writer = new CodeWriter();
+  };
+
+  JSGenerator.prototype.generateTypeCoerce = function(etype) {
+    switch (etype) {
+    case "i32":
+      this.writer.out("|0");
+      break;
+    default:
+      throw etype;
+    }
+  };
+
+  JSGenerator.prototype.generateLocalName = function(index) {
+    this.writer.out("l").out(index);
+  };
+
   // TODO precedence.
-  var generateExpr = function(expr, writer) {
+  JSGenerator.prototype.generateExpr = function(expr) {
     switch (expr.type) {
-    case "const":
-      writer.out(expr.value);
+    case "const_i32":
+      this.writer.out(expr.value);
+      break;
+    case "const_f32":
+      this.writer.out(expr.value);
+      break;
+    case "getlocal":
+      this.generateLocalName(expr.index);
       break;
     case "binop":
-      generateExpr(expr.left, writer);
-      writer.out(" ").out(expr.op).out(" ");
-      generateExpr(expr.right, writer);
+      this.writer.out("(");
+      this.writer.out("(");
+      this.generateExpr(expr.left);
+      this.writer.out(" ").out(expr.op).out(" ");
+      this.generateExpr(expr.right);
+      this.writer.out(")");
+      this.generateTypeCoerce(expr.etype);
+      this.writer.out(")");
+      break;
+    case "calldirect":
+      this.writer.out("(");
+      this.writer.out(this.m.funcs[expr.func].name);
+      this.writer.out("(");
+      for (var i in expr.args) {
+	if (i != 0) {
+	  this.writer.out(", ")
+	}
+	this.generateExpr(expr.args[i]);
+      }
+      this.writer.out(")");
+      this.writer.out(")");
+      this.generateTypeCoerce(expr.etype);
       break;
     case "return":
-      writer.out("return ");
-      generateExpr(expr.expr, writer);
+      this.writer.out("return ");
+      this.generateExpr(expr.expr);
       break;
     default:
       console.log(expr);
@@ -76,35 +202,65 @@ var wasm = {};
     };
   };
 
-  var generateBlock = function(block, writer) {
+  JSGenerator.prototype.generateBlock = function(block) {
     for (var i in block) {
-      generateExpr(block[i], writer);
-      writer.out(";").eol();
+      this.generateExpr(block[i]);
+      this.writer.out(";").eol();
     }
   };
 
-  var generateFunc = function(func, writer) {
-    writer.out("function ").out(func.name).out("() {").eol();
-    writer.indent();
-    generateBlock(func.body, writer);
-    writer.dedent();
-    writer.out("}").eol();
+  JSGenerator.prototype.generateFunc = function(func) {
+    this.writer.out("function ").out(func.name).out("(");
+    for (var i = 0; i < func.argCount; i++) {
+      if (i != 0) {
+	this.writer.out(", ");
+      }
+      this.generateLocalName(i);
+    }
+    this.writer.out(") {").eol();
+    this.writer.indent();
+
+    for (var i = 0; i < func.argCount; i++) {
+      this.generateLocalName(i);
+      this.writer.out(" = ");
+      this.generateLocalName(i);
+      this.generateTypeCoerce(func.locals[i]);
+      this.writer.out(";").eol();
+    }
+
+    for (var i = func.argCount; i < func.locals.length; i++) {
+      this.writer.out("var ");
+      this.generateLocalName(i);
+      this.writer.out(" = 0;").eol();
+      // TODO initialize to the correct type of zero.
+    }
+
+    this.generateBlock(func.body);
+    this.writer.dedent();
+    this.writer.out("}").eol();
+  };
+
+  JSGenerator.prototype.generateModule = function(module) {
+    this.writer.out("(function() {").eol().indent();
+    for (var i in module.funcs) {
+      this.generateFunc(module.funcs[i]);
+    };
+    this.writer.out("return {").eol().indent();
+    for (var i in module.funcs) {
+      var func = module.funcs[i];
+      this.writer.out(func.name).out(": ").out(func.name).out(",").eol();
+    };
+    this.writer.dedent().out("};").eol();
+    this.writer.dedent().out("})()");
   };
 
   exports.GenerateJS = function(module) {
-    var writer = new CodeWriter();
-    writer.out("(function() {").eol().indent();
-    for (var i in module.funcs) {
-      generateFunc(module.funcs[i], writer);
-    };
-    writer.out("return {").eol().indent();
-     for (var i in module.funcs) {
-       var func = module.funcs[i];
-       writer.out(func.name).out(": ").out(func.name).out(",").eol();
-    };
-    writer.dedent().out("};").eol();
-    writer.dedent().out("})()");
-    return writer.output;    
+    var semantic = new SemanticPass();
+    semantic.processModule(module);
+
+    var gen = new JSGenerator(module);
+    gen.generateModule(module);
+    return gen.writer.output;
   };
 
 })(wasm);
